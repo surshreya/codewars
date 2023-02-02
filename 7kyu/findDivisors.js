dd106fb2e54765f487a280e558cb97a4e1ba2e4a
surshreya <surshreya.0727@gmail.com>
2023-02-02 14:11:27 +0530

    kata solutions

---
+++ 7kyu/findDivisors.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+function divisors(integer) {
+  let divisorArr = [];
+  for (let i = 2; i <= Math.sqrt(integer); i++) {
+    if (integer % i === 0) {
+      divisorArr.push(i);
+      divisorArr.push(integer / i);
+    }
+  }
+  return divisorArr.length === 0
+    ? `${integer} is prime`
+    : divisorArr.sort((a, b) => a - b);
+}
---
+++ 7kyu/validatePIN.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN(pin) {
+  if (!pin) {
+    return false;
+  }
+
+  if (pin.length !== 4 && pin.length !== 6) {
+    return false;
+  }
+
+  const splittedPIN = pin.split("");
+  return splittedPIN.every((digit) => digit >= "0" && digit <= "9");
+}
